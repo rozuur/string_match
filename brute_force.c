99b756a22fa7bfcdbf17b6118fb8c52e3946a2a7
rozuur <rozuur@gmail.com>
2011-06-27 16:19:46 +0530

    A simple brute force implementation of string matching, to compare strings strcmp is used. strcmp should be modified to compare only start string of p with q.

---
+++ brute_force.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int strcmp(const char *p, const char *q)
+{
+  while(*p && *q && *p == *q){
+    ++p;
+    ++q;
+  }
+  return *p - *q;
+}
+
+/*
+  Checks at each point if the needle is substring of haystack
+ */
+int brute_force(const char *needle, const char *haystack)
+{
+  const char *h = haystack;
+  while(*h++){
+    if(strcmp(needle, h) == 0)
+      return h - haystack;
+  }
+  return -1;
+}
+
+int main(int argc, char **argv)
+{
+  if(argc < 3){
+    printf("Usage : <command> <pattern> <string>\n");
+    return 0;
+  }
+  int index = brute_force(argv[1], argv[2]);
+  if(index >= 0)
+    printf("Found %s in %s at %d\n", argv[1], argv[2], index);
+  else
+    printf("Failed to find %s in %s\n", argv[1], argv[2]);
+  return 0;
+}
